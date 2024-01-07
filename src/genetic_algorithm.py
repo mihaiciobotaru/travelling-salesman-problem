c49abdf050cc80e9c7a10f092a6af2d710a9dc2f
Mihai Ciobotaru <mihai2096@gmail.com>
2024-01-07 11:35:52 +0200

    cleaned code and plot

--- src/genetic_algorithm.py
+++ src/genetic_algorithm.py
@@ -1,47 +1,98 @@
 import random
-from instance import Instance
+import matplotlib.pyplot as plt
 from solution import Solution
-from plot import Plotter
+from utils import Point
 
-class GeneticAlgortihm:
+DATA_FOLDER = 'C:\\Users\\ejump\\travelling-salesman-problem\\data\\'
+EPOCHS = 100
+POPULATION_SIZE = 1000
+MUTATION_PROBABILITY = 0.1
+CROSSOVER_PROBABILITY = 0.0
 
-    EPOCHS = 1000
-    POPULATION_SIZE = 1000
-    MUTATION_PROBABILITY = 0.1
-    CROSSOVER_PROBABILITY = 0.0
+instance = None
+population = None
+plotter = None
+fitness_history = []
 
-    instance = None
-    population = None
-    plotter = None
+def getInstanceFromFile(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
 
-    def __init__(self, instance: Instance):
-        self.instance = instance
-        self.population = [Solution(instance.length) for i in range(self.POPULATION_SIZE)]
-        self.plotter = Plotter()
+        for i in range(len(lines)):
+            if lines[i].strip() == 'NODE_COORD_SECTION':
+                points = []
+                for j in range(i + 1, len(lines)):
+                    if lines[j].strip() == 'EOF':
+                        break
+                    line = lines[j].strip().split()
+                    points.append([int(line[1]), int(line[2])])
+                
+                return {
+                    'length': len(points),
+                    'input': [Point(point[0], point[1]) for point in points]
+                }
+            
+def on_plot_close(event):
+    exit()
 
-    def run(self):
-        for i in range(self.EPOCHS):
-            self.population.sort(key=lambda solution: self.instance.getSolutionDistance(solution))
+def plot_fitness_history():
+    x = [i for i in range(len(fitness_history))]
+    plt.plot(x, fitness_history, 'r.:')
 
-            if i % 10 == 0:
-                print('Epoch: ', i, ' Fitness: ', self.instance.getSolutionDistance(self.population[0]))
-                self.plotter.drawGraph(self.instance.input, self.population[0].points)
+    plt.xlabel('Epoch')
+    plt.ylabel('Fitness')
+    
+def plot(solution, epoch, close=True):
+    plt.subplot(3, 2, (1, 4))
+    solution.plot(instance['input'])
+    plt.title('Epoch: ' + str(epoch) + ' Fitness: ' + str( "{:.2f}".format(fitness_history[-1])))
+    plt.subplot(3, 2, (5, 6))
+    plot_fitness_history()
 
-            new_population = []
+    if close:
+        plt.show(block=False)
+        plt.pause(0.2)
+        plt.clf()
+    else:
+        plt.show()
 
-            for j in range(int(self.POPULATION_SIZE / 2)):
-                if random.random() < self.CROSSOVER_PROBABILITY:
-                    child = self.population[j].crossover(self.population[random.randint(0, len(self.population) - 1)])
-                else:
-                    child = self.population[j]
 
-                if random.random() < self.MUTATION_PROBABILITY:
-                    child.mutate()
 
-                new_population.append(child)
 
-            self.population = new_population
+if __name__ == "__main__":
+    fig = plt.figure(figsize=(12,6))
+    fig.canvas.mpl_connect('close_event', on_plot_close)
 
-        self.population.sort(key=lambda solution: self.instance.getSolutionDistance(solution))
 
-        return self.population[0]
+    instance = getInstanceFromFile(DATA_FOLDER + 'xqf131.tsp')
+    population = [Solution(instance['length']) for i in range(POPULATION_SIZE)]
+
+    for i in range(EPOCHS):
+        population.sort(key=lambda x: x.getTotalDistance(instance['input']), reverse=True)
+        fitness = population[0].getTotalDistance(instance['input'])
+        fitness_history.append(fitness)
+
+        if i % 10 == 0:
+            print('Epoch: ', i, ' Fitness: ', fitness)
+            plot(population[0], i)
+
+        new_population = []
+
+        for j in range(POPULATION_SIZE):
+            if random.random() < CROSSOVER_PROBABILITY:
+                new_population.append(population[j].crossover(population[j + 1]))
+            else:
+                new_population.append(population[j])
+
+        for j in range(POPULATION_SIZE):
+            if random.random() < MUTATION_PROBABILITY:
+                new_population[j].mutate()
+
+        population = new_population
+
+    population.sort(key=lambda x: x.getTotalDistance(instance['input']), reverse=True)
+    print('Epoch: ', EPOCHS, ' Fitness: ', fitness_history[-1])
+
+    # dont close the plot window
+    plot(population[0], EPOCHS, close=False)
+
--- src/solution.py
+++ src/solution.py
@@ -1,36 +1,56 @@
 import random
-
+import matplotlib.pyplot as plt
+from utils import Point, pointsDistance
 
 class Solution:
     points = None
+    length = None
 
     def __init__(self, arg):
         if isinstance(arg, int):
             self.points = [i for i in range(arg)]
             random.shuffle(self.points)
+            self.length = arg
         elif isinstance(arg, list):
             self.points = arg
+            self.length = len(arg)
 
 
     def mutate(self):
-        a = random.randint(0, len(self.points) - 1)
-        b = random.randint(0, len(self.points) - 1)
+        a = random.randint(0, self.length - 1)
+        b = random.randint(0, self.length - 1)
 
         self.points[a], self.points[b] = self.points[b], self.points[a]
 
     def crossover(self, other):
-        a = random.randint(0, len(self.points) - 1)
-        b = random.randint(0, len(self.points) - 1)
+        a = random.randint(0, self.length - 1)
+        b = random.randint(0, self.length - 1)
 
         if a > b:
             a, b = b, a
 
-        child = Solution(len(self.points))
+        child = Solution(self.length)
 
         child.points = self.points[a:b]
 
-        for i in range(len(other.points)):
+        for i in range(self.length):
             if other.points[i] not in child.points:
                 child.points.append(other.points[i])
 
         return child
+    
+    def getTotalDistance(self, pointsPositions, circular = False):
+        distance = 0
+
+        for i in range(self.length) if circular else range(self.length - 1):
+            distance += pointsDistance(pointsPositions[self.points[i]], pointsPositions[self.points[(i + 1) % self.length]])
+
+        return distance
+    
+    def plot(self, pointsPositions):
+        x = [pointsPositions[self.points[i]].x for i in range(self.length)]
+        y = [pointsPositions[self.points[i]].y for i in range(self.length)]
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.plot(x, y, 'ro-')
+
